8a64fcb79d2a771869bf0505acad09d56ecff001
Sudipto Dutta <sudiptodutta_1992@outlook.com>
2022-10-08 12:59:53 +0530

    All action methods implemented for Paltform

--- PlatformService/Profiles/PlatformsProfile.cs
+++ PlatformService/Profiles/PlatformsProfile.cs
@@ -10,6 +10,7 @@ namespace PlatformService.Profiles
         {
             //Source -> Target
             CreateMap<Platform, PlatformReadDto>();
+            CreateMap<PlatformCreateDto, Platform>();
         }
     }
 }
